559b16a2ae46c4830ba939019a38cf08ec7536f3
anton <antonsbilan@gmail.com>
2023-09-26 15:00:48 +0300

    feature/UN-46 Create base UI for application

--- src/utils/localStorage.ts
+++ src/utils/localStorage.ts
@@ -1,9 +1,15 @@
 export const getSideMenuState = (): boolean | null => {
+  if (typeof window === 'undefined') {
+    return null;
+  }
   const state = localStorage?.getItem('side_menu');
 
   return state === 'true' ? true : state === 'false' ? false : null;
 };
 
 export const setSideMenuState = (state: boolean) => {
-  localStorage.setItem('side_menu', state?.toString());
+  if (typeof window === 'undefined') {
+    return;
+  }
+  localStorage?.setItem('side_menu', state?.toString());
 };
